2f005ad16b34d25b1b4cd185a3e178d24f4d7629
Boris <boris.krj@protonmail.com>
2025-05-21 18:52:11 +0200

    rasa

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,31 @@
 <script lang="ts">
 	export let data;
-	let images = data.images;
 
-	let prompt = '';
-	let isLoading = false;
+	let chats = data.chats;
+	let sidebarOpen = true;
 
-	async function onSubmit(event: Event) {
-		event.preventDefault();
-
-		isLoading = true;
-
-		isLoading = false;
-		prompt = '';
-	}
+	let currentChat = chats[0];
 </script>
 
-<main class="container mx-auto px-4 py-8 max-w-6xl"></main>
+<main class="flex h-screen">
+	<div class="w-64 bg-gray-200 flex flex-col {sidebarOpen ? '' : 'hidden'}">
+		{#each chats as c}
+			<div>{c.title}</div>
+		{/each}
+	</div>
+	<div class="flex-1 flex flex-col bg-cyan-700">
+		<button on:click={() => (sidebarOpen = !sidebarOpen)}>ß</button>
+
+		<div class="flex-1 overflow-y-auto space-y-4">
+			{#each currentChat.messages as m}
+				<div
+					class="flex {m.userType == 'human'
+						? 'justify-end bg-gray-400 rounded-lg'
+						: 'justify-start bg-white rounded-lg'}"
+				>
+					<div>{m.content}</div>
+				</div>
+			{/each}
+		</div>
+	</div>
+</main>
